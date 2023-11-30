ff085bd0c50963ca9a74279fa4508a9dc0ddfbb0
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-11-30 19:00:05 +0600

    Basic setUp our project

---
+++ src/__project_steps.js
@@ -0,0 +1,11 @@
+/**\
+ *   ============ Basic Project setUp =============
+ * 1. Create firebase app
+ * 2. Get firebase configuration inside firebase config.js
+ * 3. Make sure to export app from firebase config
+ * 4. Create a react app
+ * 5. Install firebase and react router dom
+ * 6. Install bootstrap and react bootstrap
+ * 7. Make sure to import bootstrap css the index.js file
+ * 8.
+ */
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjDlDjdKpdqqUi8eKw7h6izsofws9nvkg",
+  authDomain: "dragon-news-89961.firebaseapp.com",
+  projectId: "dragon-news-89961",
+  storageBucket: "dragon-news-89961.appspot.com",
+  messagingSenderId: "502667627231",
+  appId: "1:502667627231:web:483231a8e395d00867b76a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
