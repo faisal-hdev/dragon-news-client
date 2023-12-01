af6f714acfd87c68ef2fc15a777f4ba905a86a27
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-12-01 21:39:20 +0600

    Dynamically data loadded and social site icon reactIcon design

---
+++ src/Pages/Shared/BrandCarousel/BrandCarousel.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import ExampleCarouselImage from "components/ExampleCarouselImage";
+
+function BrandCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <ExampleCarouselImage text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default BrandCarousel;
--- src/Pages/Shared/RigthSIdeNav/RightSideNav.js
+++ src/Pages/Shared/RigthSIdeNav/RightSideNav.js
@@ -1,9 +1,61 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaWhatsapp,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa6";
+import ListGroup from "react-bootstrap/ListGroup";
+// import BrandCarousel from "../BrandCarousel/BrandCarousel";
 
 const RightSideNav = () => {
   return (
     <div>
-      <h2>Right side nav</h2>
+      <ButtonGroup vertical>
+        <Button className="mb-2" variant="outline-primary">
+          {/* <FaGoogle className="mr-2" /> */}
+          <FaGoogle className="m-2"></FaGoogle>
+          Login with Google
+        </Button>
+        <Button variant="outline-dark">
+          <FaGithub className="m-2" />
+          Login with GitHub
+        </Button>
+      </ButtonGroup>
+      <div>
+        <h6 className="mt-4">Find Us on</h6>
+        <ListGroup as="ol">
+          <ListGroup.Item className="mb-2">
+            <Button variant="link">
+              <FaFacebook className="m-1" />
+              Facebook
+            </Button>
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <Button variant="link">
+              <FaTwitter className="m-1" />
+              Twitter
+            </Button>
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <Button variant="link">
+              <FaWhatsapp className="m-1" />
+              WhatsApp
+            </Button>
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <Button variant="link">
+              <FaInstagram className="m-1" />
+              Facebook
+            </Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>{/* <BrandCarousel /> */}</div>
     </div>
   );
 };
