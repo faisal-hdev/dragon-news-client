ae0db60d27a7cbbf394e1a05c472288a6073b46a
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-12-01 19:50:27 +0600

    Router outlet setup complete

---
+++ src/Pages/News/News/News.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const News = () => {
+  return (
+    <div>
+      <h2>This is news Compo</h2>
+    </div>
+  );
+};
+
+export default News;
--- src/layOut/Main.js
+++ src/layOut/Main.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import LeftSideNav from "../Pages/Shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../Pages/Shared/RigthSIdeNav/RightSideNav";
+import Header from "../Pages/Shared/Header/Header";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
-      <Outlet></Outlet>
+      <Header />
+      <Container>
+        <Row>
+          <Col lg="2">
+            <LeftSideNav />
+          </Col>
+          <Col lg="7">
+            <Outlet></Outlet>
+          </Col>
+          <Col lg="3">
+            <RightSideNav />
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
     </div>
   );
 };
